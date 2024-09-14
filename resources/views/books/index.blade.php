3a37cd3d1ada5ae495ed388be4f84a2f32f983f1
mickey <mickeyjiyestha@gmail.com>
2024-09-14 12:32:51 +0800

    Perbaikan

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -2,9 +2,31 @@
 
 @section('container')
 
-<table class="table table-bordered">
+
+<form method="GET" action="{{ route('books.index') }}">
+    <div class="form-group">
+        <label for="list">List shown :</label>
+        <select name="list" id="list" class="form-control">
+            @for ($i = 10; $i <= 100; $i += 10)
+                <option value="{{ $i }}" {{ $listShow == $i ? 'selected' : '' }}>{{ $i }}</option>
+            @endfor
+        </select>
+    </div>
+
+    <div class="form-group">
+
+        <label for="search">Search :</label>
+        <input type="text" name="search" id="search" value="{{ $search }}" class="form-control">
+    </div>
+
+    <button type="submit" class="btn btn-primary">SUBMIT</button>
+</form>
+
+
+<table class="table table-bordered mt-3">
     <thead class="table-dark">
     <tr>
+        <td>No</td>
         <td>Book Name</td>
         <td>Category Name</td>
         <td>Author Name</td>
@@ -12,16 +34,20 @@
         <td>Voter</td>
     </tr>
     </thead>
-    @foreach ($book as $b)
+    <tbody>
+    @foreach ($books as $index => $b)
         <tr>
+        <td>{{ $loop->iteration }}</td>
         <td>{{ $b->name }}</td>
         <td>{{ $b->category->name }}</td>
         <td>{{ $b->author->author }}</td>
         <td>{{ $b->average_rating }}</td>
         <td>{{ $b->voter }}</td>
         </tr>
-        @endforeach
-    
+    @endforeach
+    </tbody>
 </table>
+
+
 
 @endsection
